08b549e171c67efddb640085dd5db1e406af98ec
Arfazrll <azril4974@gmail.com>
2025-10-19 21:56:39 +0700

    UpdateSetupMigration

---
+++ belaravel/app/Models/ReturnBookCheck.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ReturnBookCheck extends Model
+{
+    protected $fillable = [
+        'return_book_id',
+        'condition',
+        'notes',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'condition' => ReturnBookStatus::class,
+        ];
+    }
+}
